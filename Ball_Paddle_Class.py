b66a43c0d8046559c393e08fc7c2523a089169a1
rheaj <rheaj@berea.edu>
2025-04-11 10:16:01 -0400

    somethings wrong, indentation error with class ball

--- Ball_Paddle_Class.py
+++ Ball_Paddle_Class.py
@@ -6,15 +6,33 @@
 #######################################################################################
 
 from turtle import *
+from pygame import *
+from tkinter import *
 
-class Ball():
-
-    def __init__(self):
-
-        self.color = "Blue"
-        self.size = 10
-        self.turtle = None
-        self.loc = (0,0)
-        self.speed = 3
+    class Ball():
 
 
+
+        def __init__(self):
+
+            self.color = "Blue"
+            self.size = 5
+            self.turtle = None
+            self.loc = sety(0) and setx(0)
+            self.speed = 3
+
+
+
+
+    class Paddle():
+
+        def __init__(self):
+            self.color = "White"
+            self.size = 25
+            self.turtle = None
+            self.loc =
+            self.u_key = 25
+            self.dwn_key = 28
+
+
+print(dict.keys())
