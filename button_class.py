df0c69b14b64f2a9380ae7243e73e970c37dd1dd
magnus-mccaslin <mccaslina@berea.edu>
2025-04-14 10:16:21 -0400

    Putting button code into game.py

--- button_class.py
+++ button_class.py
@@ -7,20 +7,21 @@
 # Purpose: Create the class for the button objects
 ######################################################################
 
-class Button():
+class Button:
 
 
-    def __init__(self, pos: tuple, size, message, event):
+    def __init__(self, pos: tuple, size: tuple, message):
         self.posx = pos[0]
         self.posy = pos[1]
-        self.size = size
+        self.height = size[0]
+        self.width = size[1]
         self.color = (255, 255, 255)
         self.color_light = (170, 170, 170)
         self.color_dark = (100, 100, 100)
         self.message = message
-        self.event = event
 
     def start_game(self):
+        pass
 
 
 
--- game.py
+++ game.py
@@ -7,7 +7,7 @@
 # Purpose: Runs a Pong game
 ######################################################################
 import pygame
-import sys
+from button_class import Button
 
 class Game:
 
@@ -24,12 +24,10 @@ class Game:
         self.height = self.screen.get_height()
         self.bg_color = (0, 64, 0)
 
-    def title_screen(self):
-        pass
 
 
 
-    def run(self):
+    def title_run(self):
         """
         Runs the game forever
 
@@ -39,16 +37,24 @@ class Game:
             # Handle game ending first
             self.screen.fill(self.bg_color)
             mouse = pygame.mouse.get_pos()
+            quit_button = Button((0, 0), (40, 140), 'Quit')
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    if self.width/2 <= mouse[0] <= self.width/2+140 and self.height/2 <= mouse[1] <= height/2+40:
-                        pygame.draw.rect(self.screen, self.color)
+                    if quit_button.posx <= mouse[0] <= quit_button.posx+140 and quit_button.posy <= mouse[1] <= quit_button.posy+40:
+                        pygame.quit()
 
+            self.screen.fill(self.bg_color)
+            mouse = pygame.mouse.get_pos()
+            if width / 2 <= mouse[0] <= width / 2 + 140 and height / 2 <= mouse[1] <= height / 2 + 40:
+                pygame.draw.rect(screen, color_light, [width / 2, height / 2, 140, 40])
+            else:
+                pygame.draw.rect(screen, color_dark, [width / 2, height / 2, 140, 40])
 
-            pygame.display.flip()
+            screen.blit(text, (width / 2 + 50, height / 2))
+            pygame.display.update()
         pygame.quit()
 
 
@@ -56,12 +62,12 @@ class Game:
 
 def main():
     """
-    Starts the cat game.
+    Starts the pong game.
 
     :return: None
     """
     game = Game()
-    game.run()
+    game.title_run()
 
 if __name__ == "__main__":
     main()
