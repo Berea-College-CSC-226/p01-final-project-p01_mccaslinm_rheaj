de3000ea4a88a7454ba6f56b57839177c2661f53
rheaj <rheaj@berea.edu>
2025-04-17 09:32:19 -0400

    paddle movement - unfinished

--- Ball_Paddle_Class.py
+++ Ball_Paddle_Class.py
@@ -5,47 +5,115 @@
 #
 #######################################################################################
 
-from turtle import *
+import pygame
+
+    #
+    #
+    # class Ball():
+    #
+    #
+    #
+    #     def __init__(self):
+    #
+    #         self.color = "Blue"
+    #         self.size = 5
+    #         self.turtle = None
+    #         self.loc = (0) and (0)
+    #         self.speed = 3
+
+
 
 import pygame
-from pygame import *
-from tkinter import *
 
-    class Ball():
+class Paddle(pygame.sprite.Sprite):
+    def __init__(self, screen):
+        super().__init__()
+        self.screen_size = screen
+        self.surf = pygame.image.load('image/Vertical_Rectangle_Black_Bordered-400x533.jpg').convert_alpha()
+        self.surf.set_colorkey((255, 255, 255), pygame.RLEACCEL)
+        self.rect = self.surf.get_rect()
+        self.rect.center = (
+            self.screen_size.get_width() // 2,
+            self.screen_size.get_height() // 2
+        )
 
-
-
-        def __init__(self):
-
-            self.color = "Blue"
-            self.size = 5
-            self.turtle = None
-            self.loc = sety(0) and setx(0)
-            self.speed = 3
-
-
-
-
-    class Paddle():
-
-        def __init__(self):
-            self.color = "White"
-            self.size = 25
-            self.turtle = None
+    def PaddleMovement(self, keys):
+        pass
 
 
+class PlayerPaddle(Paddle):
+    def __init__(self, screen):
+        super().__init__(screen)
+
+    def PaddleMovement(self, keys):
+        if keys[pygame.K_UP]:
+            self.rect.move_ip(0, -3)
+        if keys[pygame.K_DOWN]:
+            self.rect.move_ip(0, 3)
+        if keys[pygame.K_RIGHT]:
+            self.rect.move_ip(3, 0)
+        if keys[pygame.K_LEFT]:
+            self.rect.move_ip(-3, 0)
 
 
-        def PaddleMovement():
+class OpponentPaddle(Paddle):
+    def __init__(self, screen):
+        super().__init__(screen)
+        # Position the opponent on the left side of the screen
+        self.rect.midleft = (10, screen.get_height() // 2)
 
-            inputkeys = pygame.key.get_pressed()
+    def PaddleMovement(self, keys):
+        if keys[pygame.K_w]:
+            self.rect.move_ip(0, -3)  # Move up
+        if keys[pygame.K_s]:
+            self.rect.move_ip(0, 3)  # Move down
+        if keys[pygame.K_d]:
+            self.rect.move_ip(3, 0)  # Move right
+        if keys[pygame.K_a]:
+            self.rect.move_ip(-3, 0)  # Move left
 
-            if inputkeys[pygame.KEYUP]:
-                y += speed(5)
 
-            if inputkeys[pygame.KEYDOWN]:
-                y -= speed(5)
+class Game:
+    def __init__(self):
+        self.size = 800, 600
+        self.running = True
+        pygame.init()
+        self.screen = pygame.display.set_mode(self.size)
+        self.clock = pygame.time.Clock()
+        self.player = PlayerPaddle(self.screen)
+        self.opponent = OpponentPaddle(self.screen)
+
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            keys = pygame.key.get_pressed()
+            self.player.PaddleMovement(keys)
+            self.opponent.PaddleMovement(keys)
+
+            self.screen.fill('#9CBEBA')
+            self.screen.blit(self.player.surf, self.player.rect)
+            self.screen.blit(self.opponent.surf, self.opponent.rect)
+            pygame.display.update()
+            self.clock.tick(60)
+
+        pygame.quit()
+
+
+def main():
+    game = Game()
+    game.run()
+
+if __name__ == "__main__":
+    main()
 
 
 
+def main():
+    game = Game()
+    game.run()
 
+if __name__ == "__main__":
+    main()
