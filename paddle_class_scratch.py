28462053dbb84c778a6283f09c2f01509eb10c41
magnus-mccaslin <mccaslina@berea.edu>
2025-04-21 10:25:05 -0400

    made alternative paddle class code in scratch file

--- paddle_class_scratch.py
+++ paddle_class_scratch.py
@@ -1,9 +1,3 @@
-#######################################################################################
-#Author: Jaron Rhea
-#
-#
-#
-#######################################################################################
 
 import pygame
 
@@ -25,39 +19,35 @@ import pygame
 
 import pygame
 
-class Paddle(pygame.sprite.Sprite):
-    def __init__(self, screen):
-        super().__init__()
-        self.screen_size = screen
-        self.surf = pygame.image.load('image/Vertical_Rectangle_Black_Bordered-400x533.jpg').convert_alpha()
-        self.surf.set_colorkey((255, 255, 255), pygame.RLEACCEL)
+class Paddle():
+    def __init__(self, screen, pos: tuple):
+        # Rect Properties
+        self.rect_color = (255, 255, 255)
+        self.rect_x = pos[0]
+        self.rect_y = pos[1]
+        self.rect_height = 140
+        self.rect_width = 40
+        self.rect_pos = (self.rect_x, self.rect_y, self.rect_width, self.rect_height)
+        self.surf = pygame.draw.rect(screen, self.rect_color, self.rect_pos, 0)
         self.rect = self.surf.get_rect()
-        self.rect.center = (
-            self.screen_size.get_width() // 2,
-            self.screen_size.get_height() // 2
-        )
-
-    def PaddleMovement(self, keys):
-        pass
-
 
 class PlayerPaddle(Paddle):
-    def __init__(self, screen):
-        super().__init__(screen)
+    def __init__(self, screen, pos: tuple):
+        super().__init__(screen, pos)
 
     def PaddleMovement(self, keys):
         if keys[pygame.K_UP]:
-            self.rect.move_ip(0, -3)
+            self.rect_y.move_up(-3)
         if keys[pygame.K_DOWN]:
-            self.rect.move_ip(0, 3)
+            self.rect_y.move_up(3)
 
 
 
 class OpponentPaddle(Paddle):
-    def __init__(self, screen):
-        super().__init__(screen)
-        # Position the opponent on the left side of the screen
-        self.rect.midleft = (10, screen.get_height() // 2)
+    def __init__(self, screen, pos: tuple):
+        super().__init__(screen, pos)
+
+
 
     def PaddleMovement(self, keys):
         if keys[pygame.K_w]:
@@ -76,8 +66,9 @@ class Game:
         self.width = self.screen.get_width()
         self.height = self.screen.get_height()
         self.clock = pygame.time.Clock()
-        self.player = PlayerPaddle(self.screen, (self.width/2 - 300, self.height/2))
-        self.opponent = OpponentPaddle(self.screen, (self.width/2 + 300, self.height/2))
+        self.screen.fill('#9CBEBA')
+        self.player = PlayerPaddle(self.screen, (self.width / 2 - 300, self.height / 2))
+        self.opponent = OpponentPaddle(self.screen, (self.width / 2 + 300, self.height / 2))
 
     def run(self):
         while self.running:
@@ -85,26 +76,19 @@ class Game:
                 if event.type == pygame.QUIT:
                     self.running = False
 
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.player.PaddleMovement(keys)
+                    self.opponent.PaddleMovement(keys)
+
             keys = pygame.key.get_pressed()
             self.player.PaddleMovement(keys)
             self.opponent.PaddleMovement(keys)
 
-            self.screen.fill('#9CBEBA')
-            self.screen.blit(self.player.surf, self.player.rect)
-            self.screen.blit(self.opponent.surf, self.opponent.rect)
+
             pygame.display.update()
             self.clock.tick(60)
 
         pygame.quit()
-
-
-def main():
-    game = Game()
-    game.run()
-
-if __name__ == "__main__":
-    main()
-
 
 
 def main():
